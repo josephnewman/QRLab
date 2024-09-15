e8748d02c5b9a617034c7f051513187e0c7339ca
Joseph Newman <josephrnewman@outlook.com>
2024-09-15 16:59:31 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, flash
+import os
+import qr
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    qr_img_bytes = None
+    request_info = None
+    if request.method == 'POST' and all(x in request.form for x in ['qrdata', 'colour', 'bg_colour']):
+        request_info = {
+            'qr_data': request.form['qrdata'],
+            'colour': request.form['colour'], 
+            'bg_colour': request.form['bg_colour']
+        }
+        try:
+            qr_img_bytes = qr.make_qr(
+                data = request_info['qr_data'],
+                colour=request_info['colour'],
+                bg_colour=request_info['bg_colour']
+            )
+        except:
+            flash('An unexpected error occurred. Please try again.')
+
+    return render_template('index.html', qr_img_bytes = qr_img_bytes, request_info = request_info)
+
+
+if __name__ == '__main__':
+    app.run(
+        debug = True,
+    )
---
+++ qr.py
@@ -0,0 +1,36 @@
+import qrcode
+import io
+import base64
+
+def make_qr(data = ':)', colour="black", bg_colour="white"):
+    my_qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    my_qr.add_data(data)
+    my_qr.make(fit=True)
+    qr_img = my_qr.make_image(
+        fill_color = colour,
+        back_color = bg_colour,
+    )
+    byte_storage = io.BytesIO()
+    qr_img.save(byte_storage)
+
+    qr_bytes = byte_storage.getvalue()
+    display_bytes = base64.b64encode(qr_bytes).decode('utf-8')
+    return display_bytes
+
+
+''' archived simpler function
+def make_qr(data = ':)'):
+    
+    my_qr = qrcode.make(data)
+    byte_storage = io.BytesIO()
+    my_qr.save(byte_storage)
+
+    qr_bytes = byte_storage.getvalue()
+    display_bytes = base64.b64encode(qr_bytes).decode('utf-8')
+    return display_bytes
+'''
